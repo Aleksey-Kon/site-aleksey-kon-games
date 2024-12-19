578de4e73ba1d22417df4e73afde573a4cd96288
Aleksey-Kon <windowsviser@gmail.com>
2024-12-19 23:34:09 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const header = document.getElementById('main-header');
+        let lastScrollY = window.scrollY;
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > lastScrollY) {
+                header.classList.add('hidden');
+            } else {
+                header.classList.remove('hidden');
+            }
+            lastScrollY = window.scrollY;
+        });
+
+
+
+        function enlargeImage(img) {
+            const modal = document.createElement('div');
+            modal.style.position = 'fixed';
+            modal.style.top = '0';
+            modal.style.left = '0';
+            modal.style.width = '100%';
+            modal.style.height = '100%';
+            modal.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+            modal.style.display = 'flex';
+            modal.style.justifyContent = 'center';
+            modal.style.alignItems = 'center';
+            modal.style.zIndex = '1000';
+        
+            const enlargedImg = document.createElement('img');
+            enlargedImg.src = img.src;
+            enlargedImg.style.maxWidth = '90%';
+            enlargedImg.style.maxHeight = '90%';
+            enlargedImg.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.5)';
+        
+            const closeButton = document.createElement('div');
+            closeButton.innerHTML = '&times;';
+            closeButton.style.position = 'absolute';
+            closeButton.style.top = '10px';
+            closeButton.style.right = '10px';
+            closeButton.style.fontSize = '30px';
+            closeButton.style.color = 'orange';
+            closeButton.style.cursor = 'pointer';
+            closeButton.style.zIndex = '1001';
+            closeButton.onclick = function() {
+                document.body.removeChild(modal);
+            };
+        
+            modal.appendChild(enlargedImg);
+            modal.appendChild(closeButton);
+        
+            document.body.appendChild(modal);
+        }
