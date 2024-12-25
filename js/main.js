3072a175967f438a7a64ee17eb941ff77d0beb21
Aleksey-Kon <windowsviser@gmail.com>
2024-12-25 21:11:27 +0300

    25.12

--- js/main.js
+++ js/main.js
@@ -84,3 +84,39 @@ const header = document.getElementById('main-header');
         detectLanguage(); // Ваш код для определения языка
       });
         
+
+
+
+
+
+    //снег
+    const snowContainer = document.querySelector('.snow-container');
+        const snowflakeCount = 100; // Количество снежинок
+        const snowflakeCharacters = ["❄", "✼", "❅", "*", "✻"];
+
+        function createSnowflake() {
+            const snowflake = document.createElement("div");
+            snowflake.classList.add("snowflake");
+            snowflake.textContent = snowflakeCharacters[Math.floor(Math.random() * snowflakeCharacters.length)];
+            snowflake.style.left = Math.random() * 100 + "vw";
+            snowflake.style.animationDuration = Math.random() * 3 + 2 + "s";
+            snowflake.style.fontSize = Math.random() * 10 + 10 + "px";
+            snowflake.style.opacity = Math.random();
+            snowContainer.appendChild(snowflake);
+
+            // Удаляем снежинку, когда анимация завершена
+            snowflake.addEventListener("animationend", () => {
+                snowflake.remove();
+            });
+        }
+
+        function generateSnow() {
+            for (let i = 0; i < snowflakeCount; i++) {
+                setTimeout(createSnowflake, i * 100);
+            }
+
+            // Создаем снежинки каждые полсекунды
+            setInterval(createSnowflake, 500);
+        }
+
+        generateSnow();
